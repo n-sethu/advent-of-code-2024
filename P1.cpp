7e9bbb55be36ccfed4e37810040192d6b28fadaf
Nikhil <107327738+n-sethu@users.noreply.github.com>
2025-11-27 23:18:37 -0500

    part 1 complete

--- P1.cpp
+++ P1.cpp
@@ -1,5 +1,8 @@
 #include <algorithm>
 #include <iostream>
+#include <iostream> 
+#include <fstream>  
+#include <string>
 // #include <bitset>
 // #include <chrono>
 // #include <random>
@@ -39,8 +42,34 @@ int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 vector<vb> vis(1000, vb(1000));
 
-int main(){
-ios_base::sync_with_stdio(0);
-cin.tie(0);
+int main() {
+//   ./a < input1.txt > output1.txt
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ifstream inputFile("input1.txt");
+    vl l1, l2;
+    if (inputFile.is_open())
+    { 
+       string line;
 
+        while (std::getline(inputFile, line)) {
+          ll a = stoll(line.substr(0, line.find(" "))), b = stoll(line.substr(line.find(" ")));
+          l1.push_back(a);
+          l2.push_back(b);
+        }
+        sort(A(l1));
+        sort(A(l2));
+        ll n = l1.size();
+        ll total{};
+        for (int i = 0; i < n; i++) {
+          total += abs(l1[i]-l2[i]);
+        //   cout<<total<<'\n';
+        }
+        cout<<total;
+        inputFile.close(); 
+    }
+    else
+    {
+        std::cerr << "Error: Unable to open file." << std::endl;
+    }
 }
